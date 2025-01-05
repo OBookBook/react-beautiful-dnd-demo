19710629bb2f48432b422129edfe4200f03f3932
N.I <iworksWeb@gmail.com>
2025-01-05 23:07:25 +0900

    add TestDragDrop component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import TestDragDrop from "./components/sandbox/TestDragDrop";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <TestDragDrop />
+    </>
+  );
 }
 
 export default App;
