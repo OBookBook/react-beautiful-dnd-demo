9009421817192d6d19ac7e0a3cccdc55bd9fa58a
N.I <iworksWeb@gmail.com>
2025-01-05 22:11:39 +0900

    Array.splice()

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Filter from "./components/Filter";
 import Map from "./components/Map";
+import Splice from "./components/Splice";
 
 function App() {
   return (
     <>
       {/* <Map /> */}
-      <Filter />
+      {/* <Filter /> */}
+      <Splice />
     </>
   );
 }
