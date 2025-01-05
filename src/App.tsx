868a8b55ef461635b6a3aa2b6a7a9d4501412a2f
N.I <iworksWeb@gmail.com>
2025-01-05 23:27:30 +0900

    add dragDrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
 import "./App.css";
-import TestDragDrop from "./components/sandbox/TestDragDrop";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function App() {
   return (
     <>
-      <TestDragDrop />
+      <div className="dragDropArea">
+        <DragDropContext>
+          <Droppable droppableId="draggable">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                <Draggable draggableId="item0" index={0}>
+                  {(provided) => (
+                    <div
+                      className="item"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      item0
+                    </div>
+                  )}
+                </Draggable>
+                <Draggable draggableId="item1" index={1}>
+                  {(provided) => (
+                    <div
+                      className="item"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      item1
+                    </div>
+                  )}
+                </Draggable>
+                <Draggable draggableId="item2" index={2}>
+                  {(provided) => (
+                    <div
+                      className="item"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      item2
+                    </div>
+                  )}
+                </Draggable>
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </>
   );
 }
