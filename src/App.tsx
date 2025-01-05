9f4a98ddd8f1ed2b811cb000ed3a9d0d4bd7a497
N.I <iworksWeb@gmail.com>
2025-01-05 22:41:13 +0900

    npm i react-beautiful-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import "./App.css";
-// import Filter from "./components/Filter";
-// import Map from "./components/Map";
-// import Splice from "./components/Splice";
-// import ConditionalOperator from "./components/ConditionalOperator";
-import SpreadsheetSyntax from "./components/SpreadsheetSyntax";
 
 function App() {
-  return (
-    <>
-      {/* <Map /> */}
-      {/* <Filter /> */}
-      {/* <Splice /> */}
-      {/* <ConditionalOperator /> */}
-      <SpreadsheetSyntax />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
