f7c48557af9e2599be5fee2db4918da6c97a8dbe
N.I <iworksWeb@gmail.com>
2025-01-05 22:17:15 +0900

    add ConditionalOperator

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
-import Filter from "./components/Filter";
-import Map from "./components/Map";
-import Splice from "./components/Splice";
+// import Filter from "./components/Filter";
+// import Map from "./components/Map";
+// import Splice from "./components/Splice";
+import ConditionalOperator from "./components/ConditionalOperator";
 
 function App() {
   return (
     <>
       {/* <Map /> */}
       {/* <Filter /> */}
-      <Splice />
+      {/* <Splice /> */}
+      <ConditionalOperator />
     </>
   );
 }
---
+++ src/components/ConditionalOperator.tsx
@@ -0,0 +1,10 @@
+const ConditionalOperator = () => {
+  return (
+    <div>
+      <h1>三項演算子</h1>
+      {true ? <h2>true</h2> : <h2>false</h2>}
+    </div>
+  );
+};
+
+export default ConditionalOperator;
